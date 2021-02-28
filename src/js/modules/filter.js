a980de754d1a988e4a4b6ccca59cf6b95fdd43ac
RommanSh <rshatskih@gmail.com>
2021-02-28 15:24:08 +0300

    the filter module is finished

---
+++ src/js/modules/filter.js
@@ -0,0 +1,55 @@
+const filter = () => {
+    const menu = document.querySelector('.portfolio-menu'),
+          items = menu.querySelectorAll('li'),
+          wrapper = document.querySelector('.portfolio-wrapper'),
+          markAll = wrapper.querySelectorAll('.all'),
+          no = document.querySelector('.portfolio-no ');
+
+    const typeFilter = (markType) => {
+        markAll.forEach(mark => {
+            mark.style.display = 'none';
+            mark.classList.remove('animated', 'fadeIn');
+        });
+
+        no.style.display = 'none';
+        no.classList.remove('animated', 'fadeIn');
+
+        if (markType.length > 0) {
+            markType.forEach(mark => {
+                mark.style.display = 'block';
+                mark.classList.add('animated', 'fadeIn');
+            });
+        } else {
+            no.style.display = 'block';
+            no.classList.add('animated', 'fadeIn');
+        }
+    };
+
+    const showBlock = (selector) => {
+        let btn = menu.querySelector(selector),
+            block = wrapper.querySelectorAll(selector);
+
+        btn.addEventListener('click', () => {
+            typeFilter(block);
+        });
+    };
+
+    showBlock('.all');
+    showBlock('.lovers');
+    showBlock('.chef');
+    showBlock('.girl');
+    showBlock('.guy');
+    showBlock('.grandmother');
+    showBlock('.granddad');
+
+    menu.addEventListener('click', (e) => {
+        let target = e.target;
+
+        if (target && target.tagName == 'LI') {
+            items.forEach(btn => btn.classList.remove('active'));
+            target.classList.add('active');
+        }
+    });
+};
+
+export default filter;
